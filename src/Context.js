4c81959a5f9002a023cbe8a9cb2f981b05feb4c8
pchessah <paulchesa1@gmail.com>
2020-07-07 18:45:08 +0300

    filtering of placeholders

--- src/Context.js
+++ src/Context.js
@@ -8,16 +8,30 @@ class VesselProvider extends Component {
     sortedVessels: [],
     featuredVessels: [],
     loading: true,
+    type: "all",
+    capacity: 1,
+    price: 0,
+    minPrice: 0,
+    maxPrice: 0,
+    minSize: 0,
+    maxSize: 0,
+    breakfast: false,
+    pets: false,
   };
 
   componentDidMount() {
     let vessels = this.formatData(items);
     let featuredVessels = vessels.filter((vessel) => vessel.featured === true);
+    let maxPrice = Math.max(...vessels.map((item) => item.price));
+    let maxSize = Math.max(...vessels.map((item) => item.size));
     this.setState({
       vessels,
       featuredVessels,
       sortedVessels: vessels,
       loading: false,
+      price: maxPrice,
+      maxPrice,
+      maxSize,
     });
   }
 
@@ -38,10 +52,82 @@ class VesselProvider extends Component {
     return vessel;
   };
 
+  handleChange = (event) => {
+    const target = event.target;
+    const value = target.type === "checkbox" ? target.checked : target.value;
+    const name = event.target.name;
+
+    this.setState(
+      {
+        [name]: value,
+      },
+      this.filterVessels
+    );
+  };
+
+  filterVessels = () => {
+    let {
+      vessels,
+      type,
+      capacity,
+      price,
+      maxSize,
+      minSize,
+      breakfast,
+      pets,
+    } = this.state;
+
+    //all the vessels
+    let tempVessels = [...vessels];
+
+    //transform value
+    capacity = parseInt(capacity);
+    price = parseInt(price);
+
+    //filter by type
+    if (type !== "all") {
+      tempVessels = tempVessels.filter((vessel) => vessel.type === type);
+    }
+
+    //filter by capacity
+    if (capacity !== 1) {
+      tempVessels = tempVessels.filter((vessel) => vessel.capcity >= capacity);
+    }
+
+    //filter by price
+    tempVessels = tempVessels.filter((vessel) => vessel.price <= price);
+
+    //filter by size
+    tempVessels = tempVessels.filter(
+      (vessel) => vessel.size >= minSize && vessel.size <= maxSize
+    );
+
+    //filter by breakfast
+    if (breakfast) {
+      tempVessels = tempVessels.filter((vessel) => vessel.breakfast === true);
+    }
+
+    //filter by pets
+    if (pets) {
+      tempVessels = tempVessels.filter((vessel) => vessel.pets === true);
+    }
+
+    //change state
+    this.setState({
+      
+      sortedVessels: tempVessels
+      
+    });
+  };
+
   render() {
     return (
       <VesselContext.Provider
-        value={{ ...this.state, getVessel: this.getVessel }}
+        value={{
+          ...this.state,
+          getVessel: this.getVessel,
+          handleChange: this.handleChange,
+        }}
       >
         {this.props.children}
       </VesselContext.Provider>
--- src/components/VesselsFilter/VesselsFilter.js
+++ src/components/VesselsFilter/VesselsFilter.js
@@ -1,11 +1,156 @@
-import React from 'react';
- 
-const VesselsFilter = () => {
+import React from "react";
+import { useContext } from "react";
+import { VesselContext } from "../../Context";
+import Title from "../Title/Title";
+
+//get all unique values
+const getUnique = (items, value) => {
+  return [...new Set(items.map((item) => item[value]))];
+};
+
+const VesselsFilter = ({ vessels }) => {
+  const context = useContext(VesselContext);
+  const {
+    handleChange,
+    type,
+    capacity,
+    price,
+    minPrice,
+    maxPrice,
+    minSize,
+    maxSize,
+    breakfast,
+    pets,
+  } = context;
+
+  //get unique types
+  let types = getUnique(vessels, "type");
+
+  //add all
+  types = ["all", ...types];
+
+  //map to jsx
+  types = types.map((item, index) => {
     return (
-        <div>
-            VesselsFilter
-        </div>
+      <option value={item} key={index}>
+        {item}
+      </option>
     );
-}
- 
+  });
+
+  let people = getUnique(vessels, "capacity");
+
+  people = people.map((item, index) => {
+    return (
+      <option key={index} value={item}>
+        {item}
+      </option>
+    );
+  });
+
+  return (
+    <section className="filter-container">
+      <Title title="Search Vessels" />
+      <form className="filter-form">
+        {/*select type*/}
+        <div className="form-group">
+          <label htmlFor="type">Vessel Type</label>
+          <select
+            name="type"
+            id="type"
+            value={type}
+            className="form-control"
+            onChange={handleChange}
+          >
+            {types}
+          </select>
+        </div>
+        {/*end of select type*/}
+        {/*guest*/}
+        <div className="form-group">
+          <label htmlFor="capacity">Passengers</label>
+          <select
+            name="capacity"
+            id="capacity"
+            value={capacity}
+            className="form-control"
+            onChange={handleChange}
+          >
+            {people}
+          </select>
+        </div>
+        {/*end of guest*/}
+
+        {/*vessel price*/}
+        <div className="form-group">
+          <label htmlFor="price">Vessel price: Ksh{price}</label>
+          <input
+            type="range"
+            name="price"
+            min={minPrice}
+            max={maxPrice}
+            id="price"
+            value={price}
+            onChange={handleChange}
+            className="form-control"
+          />
+        </div>
+
+        {/* end of room price*/}
+
+        {/*size*/}
+        <div className="form-group">
+          <label htmlFor="size">Vessel Size</label>
+          <div className="size-inputs">
+            <input
+              type="number"
+              name="minSize"
+              id="size"
+              value={minSize}
+              onChange={handleChange}
+              className="size-input"
+            />
+            <input
+              type="number"
+              name="maxSize"
+              id="size"
+              value={maxSize}
+              onChange={handleChange}
+              className="size-input"
+            />
+          </div>
+        </div>
+
+        {/*end of size*/}
+
+        {/*extras*/}
+        <div className="form-group">
+          <div className="single-extra">
+            <input
+              type="checkbox"
+              name="breakfast"
+              id="breakfast"
+              checked={breakfast}
+              onChange={handleChange}
+            />
+            <label htmlFor="breakfast">Breakfast</label>
+          </div>
+          <div className="single-extra">
+            <input
+              type="checkbox"
+              name="pets"
+              id="pets"
+              checked={pets}
+              onChange={handleChange}
+            />
+            <label htmlFor="pets">Pets</label>
+          </div>
+        </div>
+
+        {/* end of extras*/}
+      </form>
+    </section>
+  );
+};
+
 export default VesselsFilter;
