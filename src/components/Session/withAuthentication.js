bbcc5b0fd22784bd42f47c9941ea473027ee5f70
pchessah <paulchesa1@gmail.com>
2020-07-13 10:03:52 +0300

    roles use

--- src/components/Session/withAuthentication.js
+++ src/components/Session/withAuthentication.js
@@ -22,37 +22,6 @@ const withAuthentication = Component => {
           this.setState({ authUser: null });
         },
       );
-      this.listener = this.props.firebase.onAuthUserListener(
-        authUser => {
-          if (authUser) {
-            this.props.firebase
-              .user(authUser.uid)
-              .once('value')
-              .then(snapshot => {
-                const dbUser = snapshot.val();
- 
-                // default empty roles
-                if (!dbUser.roles) {
-                  dbUser.roles = {};
-                }
- 
-                // merge auth and db user
-                authUser = {
-                  uid: authUser.uid,
-                  email: authUser.email,
-                  ...dbUser,
-                };
- 
-                this.setState({ authUser });
-              });
-          } else {
-            this.setState({ authUser: null });
-          }
-        },
-        () => {
-          this.setState({ authUser: null });
-        },
-      );
     }
 
     componentWillUnmount() {
--- src/components/Session/withAuthorization.js
+++ src/components/Session/withAuthorization.js
@@ -1,10 +1,10 @@
-import React from 'react';
-import { withRouter } from 'react-router-dom';
-import { compose } from 'recompose';
+import React from "react";
+import { withRouter } from "react-router-dom";
+import { compose } from "recompose";
 
-import AuthUserContext from './context';
-import { withFirebase } from '../Firebase';
-import * as ROUTES from '../../constants/routes';
+import AuthUserContext from "./context";
+import { withFirebase } from "../Firebase";
+import * as ROUTES from "../../constants/routes";
 
 const withAuthorization = condition => Component => {
   class WithAuthorization extends React.Component {
@@ -12,37 +12,6 @@ const withAuthorization = condition => Component => {
       this.listener = this.props.firebase.onAuthUserListener(
         authUser => {
           if (!condition(authUser)) {
-            this.props.history.push(ROUTES.SIGN_IN);
-          }
-        },
-        () => this.props.history.push(ROUTES.SIGN_IN),
-      );
-      this.listener = this.props.firebase.onAuthUserListener(
-        authUser => {
-          if (authUser) {
-            this.props.firebase
-              .user(authUser.uid)
-              .once('value')
-              .then(snapshot => {
-                const dbUser = snapshot.val();
- 
-                // default empty roles
-                if (!dbUser.roles) {
-                  dbUser.roles = {};
-                }
- 
-                // merge auth and db user
-                authUser = {
-                  uid: authUser.uid,
-                  email: authUser.email,
-                  ...dbUser,
-                };
- 
-                if (!condition(authUser)) {
-                  this.props.history.push(ROUTES.SIGN_IN);
-                }
-              });
-          } else {
             this.props.history.push(ROUTES.SIGN_IN);
           }
         },
